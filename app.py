3fa0d63dce3d252802e2248dac34d9bd10bdeded
NolanCarbin <nolan2556@gmail.com>
2021-06-15 14:51:34 -0400

    Added cmu graphics and app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+from cmu_112_graphics import *
+
+
+def appStarted(app):
+    pass
+
+def appStopped(app):
+    pass
+
+def mousePressed(app, event):
+    pass
+
+def keyPressed(app, event):
+    pass
+
+def timerFired(app):
+    pass
+
+def redrawAll(app, canvas):
+    pass
+
+
+runApp(width=800, height=800)
