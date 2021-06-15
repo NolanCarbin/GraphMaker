ebcebafaceb83812865937e08827e9c592dbe32a
NolanCarbin <nolan2556@gmail.com>
2021-06-15 17:49:38 -0400

    Added starting node and target node, as well as buttons

--- app.py
+++ app.py
@@ -5,12 +5,38 @@ def appStarted(app):
     app.rows = 20
     app.cols = 30
     app.gridMargin = app.width // 100
+    app.startingNode = None
+    app.targetNode = None
+    app.movingStartingNode = False
+    app.movingTargetNode = False
+    app.movingWallNode = False
 
 def appStopped(app):
     pass
 
 def mousePressed(app, event):
-    pass
+    if app.movingStartingNode:
+        app.startingNode = getCell(app, event.x, event.y)  
+    elif app.movingTargetNode:
+        app.targetNode = getCell(app, event.x, event.y) 
+    inBoundsOfButtons(app, event)
+
+def inBounds(x, y, recX0, recY0, recX1, recY1):
+    return (recX0 <= x <= recX1 and 
+            recY0 <= y <= recY1)
+    
+
+def inBoundsOfButtons(app, event):
+    # (20, 20, 40, 40) starting node button
+    if inBounds(event.x, event.y, 20, 20, 40, 40): 
+        app.movingStartingNode = not app.movingStartingNode
+    # (20, 60, 40, 80) target node button
+    if inBounds(event.x, event.y, 20, 60, 40, 80): 
+        app.movingTargetNode = not app.movingTargetNode
+    # (20, 100, 40, 120) wall node button
+    if inBounds(event.x, event.y, 20, 100, 40, 120): 
+        app.movingWallNode = not app.movingWallNode
+    
 
 def keyPressed(app, event):
     pass
@@ -18,23 +44,59 @@ def keyPressed(app, event):
 def timerFired(app):
     pass
 
-def redrawAll(app, canvas):
-    for row in range(app.rows):
-        for col in range(app.cols):
-            x0, y0, x1, y1 = getCellBounds(app, row, col)
-            canvas.create_rectangle(x0, y0, x1, y1)
-
 def getCellBounds(app, row, col):
     gridWidth = app.width - app.gridMargin * 2
     gridHeight = app.height * 3/4
     cellWidth = gridWidth / app.cols
     cellHeight = gridHeight / app.rows
     x0 = col * cellWidth + app.gridMargin
-    y0 = row * cellHeight + (app.height //4 - app.gridMargin)
+    y0 = row * cellHeight + (app.height // 4 - app.gridMargin)
     x1 = x0 + cellWidth
     y1 = y0 + cellHeight
     return x0, y0, x1, y1
 
+def getCell(app, x, y):
+    gridWidth = app.width - app.gridMargin * 2
+    gridHeight = app.height * 3/4
+    cellWidth = gridWidth / app.cols
+    cellHeight = gridHeight / app.rows
+    row = int((y - (app.height // 4 - app.gridMargin)) / cellHeight)
+    col = int((x - app.gridMargin) / cellWidth)
+    if row not in range(0, app.rows) or col not in range(0, app.cols):
+        return None
+    return row, col
+
+def redrawAll(app, canvas):
+    drawGrid(app, canvas)
+    drawStartingNode(app, canvas)
+    drawTargetNode(app, canvas)
+    drawButtons(app, canvas)
+
+def drawGrid(app, canvas):
+    for row in range(app.rows):
+        for col in range(app.cols):
+            x0, y0, x1, y1 = getCellBounds(app, row, col)
+            canvas.create_rectangle(x0, y0, x1, y1)
+
+def drawStartingNode(app, canvas):
+    if app.startingNode == None: return
+    startingRow, startingCol = app.startingNode
+    x0,y0,x1,y1 = getCellBounds(app, startingRow, startingCol)
+    canvas.create_oval(x0,y0,x1,y1,fill='blue')
+
+def drawTargetNode(app, canvas):
+    if app.targetNode == None: return
+    targetRow, targetCol = app.targetNode
+    x0,y0,x1,y1 = getCellBounds(app, targetRow, targetCol)
+    canvas.create_oval(x0,y0,x1,y1,fill='green')
+
+def drawButtons(app, canvas):
+    canvas.create_rectangle(20, 20, 40, 40)
+    # canvas.create_text()
+    canvas.create_rectangle(20, 60, 40, 80)
+    # canvas.create_text()
+    canvas.create_rectangle(20, 100, 40, 120)
+    # canvas.create_text()
 
 
 runApp(width=1000, height=600)
