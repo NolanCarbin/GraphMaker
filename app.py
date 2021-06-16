11468921110ebe73a5f8ebc34ecae731b223f927
NolanCarbin <nolan2556@gmail.com>
2021-06-15 21:20:17 -0400

    added wall nodes and reset buttons

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ def appStarted(app):
     app.rows = 20
     app.cols = 30
     app.gridMargin = app.width // 100
-    app.startingNode = None
-    app.targetNode = None
+    app.startingNode = (9,9)
+    app.targetNode = (9,20)
+    app.wallNodes = set()
     app.movingStartingNode = False
     app.movingTargetNode = False
     app.movingWallNode = False
@@ -15,28 +16,57 @@ def appStopped(app):
     pass
 
 def mousePressed(app, event):
+    currentNode = getCell(app, event.x, event.y)
     if app.movingStartingNode:
-        app.startingNode = getCell(app, event.x, event.y)  
+        if currentNode != None:
+            if currentNode in app.wallNodes:
+                app.wallNodes.remove(currentNode)
+            app.startingNode = currentNode 
     elif app.movingTargetNode:
-        app.targetNode = getCell(app, event.x, event.y) 
-    inBoundsOfButtons(app, event)
+        if currentNode != None:
+            if currentNode in app.wallNodes:
+                app.wallNodes.remove(currentNode)
+            app.targetNode = currentNode
+    elif app.movingWallNode:
+        if currentNode != None:
+            if currentNode in app.wallNodes:
+                app.wallNodes.remove(currentNode)
+            elif currentNode == app.startingNode or currentNode == app.targetNode:
+                return
+            else:
+                app.wallNodes.add((currentNode))
+    inBoundsOfNodeButtons(app, event)
+    inBoundsOfResetButtons(app, event)
 
 def inBounds(x, y, recX0, recY0, recX1, recY1):
-    return (recX0 <= x <= recX1 and 
-            recY0 <= y <= recY1)
+    return (recX0 <= x <= recX1 and recY0 <= y <= recY1)
     
 
-def inBoundsOfButtons(app, event):
+def inBoundsOfNodeButtons(app, event):
     # (20, 20, 40, 40) starting node button
     if inBounds(event.x, event.y, 20, 20, 40, 40): 
         app.movingStartingNode = not app.movingStartingNode
+        if app.movingStartingNode:
+            app.movingTargetNode = False
+            app.movingWallNode = False
     # (20, 60, 40, 80) target node button
     if inBounds(event.x, event.y, 20, 60, 40, 80): 
         app.movingTargetNode = not app.movingTargetNode
+        if app.movingTargetNode:
+            app.movingStartingNode = False
+            app.movingWallNode = False
     # (20, 100, 40, 120) wall node button
     if inBounds(event.x, event.y, 20, 100, 40, 120): 
         app.movingWallNode = not app.movingWallNode
+        if app.movingWallNode:
+            app.movingStartingNode = False
+            app.movingTargetNode = False
     
+def inBoundsOfResetButtons(app, event):
+    if inBounds(event.x, event.y, app.width-140, 20, app.width-20, 60):
+        appStarted(app)
+    if inBounds(event.x, event.y, app.width-140, 80, app.width-20, 120):
+        app.wallNodes = set()
 
 def keyPressed(app, event):
     pass
@@ -63,14 +93,16 @@ def getCell(app, x, y):
     row = int((y - (app.height // 4 - app.gridMargin)) / cellHeight)
     col = int((x - app.gridMargin) / cellWidth)
     if row not in range(0, app.rows) or col not in range(0, app.cols):
-        return None
+        return 
     return row, col
 
 def redrawAll(app, canvas):
     drawGrid(app, canvas)
     drawStartingNode(app, canvas)
     drawTargetNode(app, canvas)
-    drawButtons(app, canvas)
+    drawWallNodes(app, canvas)
+    drawNodeButtons(app, canvas)
+    drawResetButtons(app, canvas)
 
 def drawGrid(app, canvas):
     for row in range(app.rows):
@@ -90,13 +122,38 @@ def drawTargetNode(app, canvas):
     x0,y0,x1,y1 = getCellBounds(app, targetRow, targetCol)
     canvas.create_oval(x0,y0,x1,y1,fill='green')
 
-def drawButtons(app, canvas):
-    canvas.create_rectangle(20, 20, 40, 40)
-    # canvas.create_text()
-    canvas.create_rectangle(20, 60, 40, 80)
-    # canvas.create_text()
-    canvas.create_rectangle(20, 100, 40, 120)
-    # canvas.create_text()
+def drawWallNodes(app, canvas):
+    if len(app.wallNodes) <= 0: return
+    for row, col in app.wallNodes:
+        x0,y0,x1,y1 = getCellBounds(app, row, col)
+        canvas.create_rectangle(x0,y0,x1,y1,fill='black')
 
+def drawNodeButtons(app, canvas):
+    if app.movingStartingNode:
+        startingNodeColor = 'blue'
+    else:
+        startingNodeColor = 'white'
+    canvas.create_rectangle(20, 20, 40, 40, fill=startingNodeColor)
+    canvas.create_text(50, 30, text='Starting Node', anchor='w')
+
+    if app.movingTargetNode:
+        targetNodeColor = 'green'
+    else:
+        targetNodeColor = 'white'
+    canvas.create_rectangle(20, 60, 40, 80, fill=targetNodeColor)
+    canvas.create_text(50, 70, text='Target Node', anchor='w')
+
+    if app.movingWallNode:
+        wallNodeColor = 'black'
+    else:
+        wallNodeColor = 'white'
+    canvas.create_rectangle(20, 100, 40, 120, fill=wallNodeColor)
+    canvas.create_text(50, 110, text='Wall Node', anchor='w')
+
+def drawResetButtons(app, canvas):
+    canvas.create_rectangle(app.width-140, 20, app.width-20, 60)
+    canvas.create_text(app.width-80, 40, text='Reset')
+    canvas.create_rectangle(app.width-140, 80, app.width-20, 120)
+    canvas.create_text(app.width-80, 100, text='Clear Path')
 
 runApp(width=1000, height=600)
